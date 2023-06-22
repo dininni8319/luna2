3c31c37be43e7ca17d437752922b32bfb127444c
Salvatore Dininni <s.dininni@yahoo.com>
2023-06-22 18:35:21 +0200

    implementations for the serch page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -12,17 +12,25 @@ import { TitleUserHeader } from '@/components'
 const Search = () => {
     const dispatch = useAppDispatch()
     const { isClicked, id } = useAppSelector(state => state.title)
+    console.log("🚀 ~ file: Search.tsx:15 ~ Search ~ id:", id)
     console.log(convertPath(id));
     
     const { payload } = useFetch(`${base_url}${id?convertPath(id):"/restaurant/list"}`)
 
-    console.log("🚀 ~ file: Restaurants.tsx:13 ~ Restaurants ~ payload:", payload)
+    const renderView = () => {
+        if (id === 'restaurants' || id === '') {
+           return payload?.map((restaurant: IRestaurant) => <Card restaurant={restaurant} />)
+        } else if (id === 'users') {
+          return payload?.map((user: IRestaurant) => <TitleUserHeader user={user}/>)
+        } else if (id === 'reviews') {
+            return <h3>No review was found!</h3>
+        } 
+    }
     return (
         <PageWrapper>
             <SearchNavigation />
             <TileGrid>  
-                 {/* <Card restaurant={restaurant} /> */}
-                {payload?.map((user: IRestaurant) => <TitleUserHeader user={user}/>)}
+                 {renderView()}
             </TileGrid>
         </PageWrapper>
     )
